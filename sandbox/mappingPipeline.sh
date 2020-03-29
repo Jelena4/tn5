2526790407341dc497d17e156cdfe69693333304
Jelena <scepanovic@uchicago.edu>
2020-03-29 15:09:22 -0500

    added scripts

--- sandbox/mappingPipeline.sh
+++ sandbox/mappingPipeline.sh
@@ -16,22 +16,32 @@
 
 ### What the script does
 # Trims read1, maps read1 onto the genome in single end mode; maps both reads in paired end mode
-# Converts to BAM, filters MAPQ 2, removes duplicates with picard 
+# Converts to BAM, filters MAPQ 2 (multimapers), removes duplicates from paired end with picard 
+# Separates forward and reverse strands from read1
 
 ### Trimming
 # The adapters we used are dedicated reads so no need for trimming them.
 # TSO barcode sequence (8 nt long) needs to be trimmed from read1.
-#seqtk trimfq -b 8 fastq/$1.fastq.gz | gzip - > fastq/$4.R1.trimmed.fastq.gz
+seqtk trimfq -b 8 fastq/$1.fastq.gz | gzip - > fastq/$4.R1.trimmed.fastq.gz
 
 ### Mapping
 # Read1 single end mode
-#bowtie2 -D 200 -R 3 -N 1 -L 20 -i S,1,0.50 --gbar 1 --no-unal -p 8 -x $3 -U fastq/$4.R1.trimmed.fastq.gz -S $4.R1.sam 2> $4.R1.mappingstats
+bowtie2 -D 200 -R 3 -N 1 -L 20 -i S,1,0.50 --gbar 1 --no-unal -p 8 -x $3 -U fastq/$4.R1.trimmed.fastq.gz -S $4.R1.sam 2> $4.R1.mappingstats
 # Paired end mode 
-#bowtie2 -D 200 -R 3 -N 1 -L 20 -i S,1,0.50 -p 8 -x $3 -1 fastq/$4.R1.trimmed.fastq.gz -2 fastq/$2.fastq.gz -S $4.PE.sam 2> $4.PE.mappingstats
-         
-#samtools view -bq 2 -@ 6 -o $4.R1.bam $4.R1.sam
-#samtools view -bq 2 -@ 6 -o $4.PE.bam $4.PE.sam
+bowtie2 -D 200 -R 3 -N 1 -L 20 -i S,1,0.50 -p 8 -x $3 -1 fastq/$4.R1.trimmed.fastq.gz -2 fastq/$2.fastq.gz -S $4.PE.sam 2> $4.PE.mappingstats
 
-### Separating forward and reverse strand
-samtools view -b -F 0x10 $4.R1.bam > $4.R1.forward.bam
-samtools view -b -f 0x10 $4.R1.bam > $4.R1.reverse.bam
+### Sam to Bam and filtering
+samtools view -bq 2 -@ 6 -o $4.R1.bam $4.R1.sam
+samtools view -bq 2 -@ 6 -o $4.PE.bam $4.PE.sam
+rm $4.R1.sam
+rm $4.PE.sam
+
+### Separating forward and reverse strand for read1
+#samtools view -b -F 0x10 $4.R1.bam > $4.R1.forward.bam
+#samtools view -b -f 0x10 $4.R1.bam > $4.R1.reverse.bam
+samtools view -@ 6 $4.R1.forward.bam > $4.R1.forward.sam
+samtools view -@ 6 $4.R1.reverse.bam > $4.R1.reverse.sam
+
+### Removing duplicates from PE
+java -XX:ParallelGCThreads=6 -jar /data/personal_folders/jscepanovic/picard.jar MarkDuplicates I=$4.PE.bam O=$4.rmdup.bam M=$4.marked.dup.metrics.txt REMOVE_DUPLICATES=true
+samtools view -@ 6 $4.rmdup.bam > $4.rmdup.sam
--- sandbox/tn5_rmdup_with_PE.R
+++ sandbox/tn5_rmdup_with_PE.R
@@ -1,22 +1,46 @@
+
+rm(list = ls())
+
 library(data.table)
 library(dplyr)
-bampe = fread('/home/jscepanovic/tn5_Nv_20dpf_Q2_srt_picardrmdup.sam', fill=T)
-r1 = fread('/home/jscepanovic/tn5_Nv_20dpf_S5_R1_Q2_sort_reverse.sam', fill=T)
-nrow(r1)
-header = r1[1:17,]
-r1 = r1[18:nrow(r1),]
-nrow(r1)
-sum(r1$V1 %in% bampe$V1)
-r1.filt = r1 %>% dplyr::filter(r1$V1 %in% bampe$V1)
-r1.filt.h = rbind(header,r1.filt)
-write.table(r1.filt.h, '/home/jscepanovic/tn5_Nv_20dpf_R1_Q2_reverse_filtR.sam', sep='\t', quote=F, row.names = F, col.names = F)
 
-r1 = fread('/home/jscepanovic/tn5_Nv_20dpf_S5_R1_Q2_sort_forward.sam', fill=T)
+organism = 'Nv'
+sample = '20dpf'
+strand = 'forward'
+
+tn5Dir = '/data/personal_folders/jscepanovic/tn5'
+setwd(tn5Dir)
+
+bampe = file.path(tn5Dir, 'tn5_' %>% 
+                    paste0(organism) %>% 
+                    paste(sample, sep = "_") %>% 
+                    paste0('.PE.rmdup.sam'))
+
+bampe = fread(bampe, fill = T)
+
+r1 = file.path(tn5Dir, 'tn5_' %>% 
+                 paste0(organism) %>% 
+                 paste(sample, sep = "_") %>% 
+                 paste0('.R1.') %>% 
+                 paste0(strand) %>% 
+                 paste0('.sam'))
+
+r1 = fread(r1, fill = T)
+
 nrow(r1)
 header = r1[1:17,]
 r1 = r1[18:nrow(r1),]
 nrow(r1)
+
 sum(r1$V1 %in% bampe$V1)
 r1.filt = r1 %>% dplyr::filter(r1$V1 %in% bampe$V1)
 r1.filt.h = rbind(header,r1.filt)
-write.table(r1.filt.h, '/home/jscepanovic/tn5_Nv_20dpf_R1_Q2_forward_filtR.sam', sep='\t', quote=F, row.names = F, col.names = F)
+fileName = 'tn5_' %>% 
+  paste0(organism) %>% 
+  paste(sample, sep = "_") %>% 
+  paste0('.R1.') %>% 
+  paste0(strand) %>% 
+  paste0('.filtR.sam')
+write.table(r1.filt.h, fileName, sep='\t', quote=F, row.names = F, col.names = F)
+
+
